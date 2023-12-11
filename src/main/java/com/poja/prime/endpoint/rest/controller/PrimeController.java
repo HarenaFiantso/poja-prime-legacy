cb6d9b5a9e901f64a111bfbb6e0763fddb04e451
Fiantso Harena <fiantsorav@gmail.com>
2023-12-11 23:18:24 +0300

    test: Create an integration test for PrimeController

--- src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
+++ src/main/java/com/poja/prime/endpoint/rest/controller/PrimeController.java
@@ -2,19 +2,26 @@ package com.poja.prime.endpoint.rest.controller;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/prod")
+@Setter
 public class PrimeController {
+  private SecureRandom secureRandom;
 
   @GetMapping("/new-prime")
   public String generateNewPrime() {
-    SecureRandom secureRandom = new SecureRandom();
+    if (secureRandom == null) {
+      secureRandom = new SecureRandom();
+    }
     BigInteger prime = BigInteger.probablePrime(10_000, secureRandom);
-
     return prime.toString();
   }
 }
---
+++ src/test/java/com/poja/prime/endpoint/rest/controller/PrimeControllerIT.java
@@ -0,0 +1,26 @@
+package com.poja.prime.endpoint.rest.controller;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.Mockito;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class PrimeControllerIT {
+  @Test
+  public void testGenerateNewPrime() {
+    PrimeController primeController = new PrimeController();
+
+    SecureRandom secureRandomMock = Mockito.mock(SecureRandom.class);
+
+    Mockito.when(secureRandomMock.nextInt()).thenReturn(1);
+
+    primeController.setSecureRandom(secureRandomMock);
+
+    String result = primeController.generateNewPrime();
+
+    BigInteger generatedPrime = new BigInteger(result);
+    Assertions.assertEquals(10000, generatedPrime.bitLength(), "The length of prime number generated is different of 10_000 bits");
+  }
+}
