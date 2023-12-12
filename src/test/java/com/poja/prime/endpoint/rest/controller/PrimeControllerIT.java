34c0d6aee98a4c7ec3103b0bb14e402247add965
Fiantso Harena <fiantsorav@gmail.com>
2023-12-12 10:26:18 +0300

    fix: Trying to fix the CD Compute error

--- src/test/java/com/poja/prime/endpoint/rest/controller/PrimeControllerIT.java
+++ src/test/java/com/poja/prime/endpoint/rest/controller/PrimeControllerIT.java
@@ -1,18 +1,19 @@
 package com.poja.prime.endpoint.rest.controller;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import com.poja.prime.conf.FacadeIT;
-import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-class PrimeControllerIT extends FacadeIT {
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PrimeControllerIT extends FacadeIT {
   @Autowired PrimeController primeController;
 
   @Test
   void new_prime_ok() {
-    BigInteger actual = primeController.generateNewPrime();
+    BigInteger actual = primeController.generatePrime();
 
     int certainly = (int) (99.99 * 100);
     assertTrue(actual.isProbablePrime(certainly));
